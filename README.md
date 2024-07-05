36cca20e4f988a6fdc844570fe11c433c2379bab
raunak0400 <raunakkumarjha233@gmail.com>
2024-07-05 20:44:00 +0530

    "Implement SMTP authentication"

--- README.md
+++ README.md
@@ -390,3 +390,5 @@ smtpServer.start();
 <!-- Updated on 2025-09-09 -->
 
 <!-- Updated on 2025-09-09 -->
+
+<!-- Updated on 2025-09-09 -->
--- server.js
+++ server.js
@@ -871,3 +871,5 @@ app.listen(PORT, () => {
 // Refactored on 2025-09-09
 
 // Bug fix applied on 2025-09-09
+
+// Feature added on 2025-09-09
