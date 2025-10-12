28f169de7494c2aa12e64dd4c1ac66533646cad9
Raunak Kumar Jha <151940169+raunak0400@users.noreply.github.com>
2025-10-12 14:02:20 +0530

    Remove duplicate nodemailer import

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-
+const nodemailer = require('nodemailer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
